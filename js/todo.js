e62c9a0a7481adc7463f39e82bef9aaeeea3c90f
KhineZinThaw7 <scm.khinezinthaw@gmail.com>
2021-09-09 10:16:04 +0630

    add done task

--- js/todo.js
+++ js/todo.js
@@ -7,7 +7,7 @@ var taskArr = [];
 btn.addEventListener('click', function () {
   itemValue = item.value;
   if(itemValue != "") {
-    taskArr.push({ 'task_name': itemValue });
+    taskArr.push({ 'task_name': itemValue, 'complete_status' : false });
     console.log(taskArr)
     window.localStorage.setItem("task", JSON.stringify(taskArr));
     item.value = '';
@@ -26,14 +26,35 @@ function showtask() {
   }
   let html = '';
   taskArr.forEach((item, index) => {
-    taskValue = `<td>${item.task_name}</td>`;
+    if(item.complete_status == false) {
+      complete = `  <td><button type="button" onclick="completeTask(${index})" class="btn btn-success">Done</button></td>`;
+      taskValue = `<td>${item.task_name}</td>`;
+    }else {
+      complete = `  <td><button type="button" onclick="completeTask(${index})" class="btn btn-secondary">UnFinish</button></td>`;
+      taskValue = `<td style="text-decoration: line-through">${item.task_name}</td>`;
+    }
     html += `<tr>
                   <th>${index + 1}</th>
                   ${taskValue}
+                  ${complete}
                   <td><button type="button" onclick="deleteTask(${index})" class="btn btn-danger"><i class="fa fa-trash"></i>Delete</button></td>
               </tr>`;
   });
   list.innerHTML = html;
+}
+
+//completeTask
+function completeTask(index) {
+  let storeTask = window.localStorage.getItem("task");
+  taskArr = JSON.parse(storeTask);
+  if(taskArr[index].complete_status == false){
+      taskArr[index].complete_status = true
+  }else{
+    taskArr[index].complete_status = false;
+  }
+
+  localStorage.setItem("task", JSON.stringify(taskArr));
+  showtask();
 }
 
 // deleteTask
---
+++ readme.md
@@ -0,0 +1,30 @@
+# Js To-Do-List
+
+![ToDo Image](images/to-do-js.png)
+
+- [Js To-Do-List](#js-to-do-list)
+  - [Text Input Field](#text-input-field)
+  - [Add New Button](#add-new-button)
+  - [Done Button](#done-button)
+  - [Unfinish Button](#unfinish-button)
+  - [Delete Button](#delete-button)
+
+## Text Input Field
+
+This is the text field to fill your task.
+
+## Add New Button
+
+The button is to add new task and store to localstorage.
+
+## Done Button
+
+This button is the task done.
+
+## Unfinish Button
+
+This button is the task unfinish.
+
+## Delete Button
+
+This button is to delete the task.
